e0e1af912832198aec5c99089494020e61ea0801
Spuros <spurakos98@gmail.com>
2020-10-25 05:57:34 +0200

    first commit

---
+++ data.js
@@ -0,0 +1,294 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-domatio-2",
+      "name": "Domatio 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -2.7037762046266423,
+        "pitch": 0.883116485265818,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7237225277436137,
+          "pitch": 0.5926592547699894,
+          "rotation": 0,
+          "target": "4-hall-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-domatio-3",
+      "name": "Domatio 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.91055350152239,
+        "pitch": 0.46513947057495564,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0046869356752257,
+          "pitch": 0.4633627444785908,
+          "rotation": 0,
+          "target": "4-hall-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-eisodos",
+      "name": "Eisodos",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -1.245657052787342,
+        "pitch": 0.34369287213183597,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1059471749968672,
+          "pitch": 0.26634070329549075,
+          "rotation": 0,
+          "target": "5-saloni-kouzina"
+        },
+        {
+          "yaw": 0.34262274457766395,
+          "pitch": 0.270170887893153,
+          "rotation": 0,
+          "target": "3-hall-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-hall-1",
+      "name": "Hall 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.4198253208910234,
+        "pitch": 0.501527493289263,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9596054341602027,
+          "pitch": 0.5185745132732578,
+          "rotation": 0,
+          "target": "2-eisodos"
+        },
+        {
+          "yaw": 0.6880058611257098,
+          "pitch": 0.4611098041216799,
+          "rotation": 0,
+          "target": "4-hall-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-hall-2",
+      "name": "Hall 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -0.6850542102013186,
+        "pitch": 0.4741538895211761,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.3074818283357175,
+          "pitch": 0.6099404718741823,
+          "rotation": 0,
+          "target": "0-domatio-2"
+        },
+        {
+          "yaw": 0.012815504858547655,
+          "pitch": 0.21945070367006458,
+          "rotation": 0,
+          "target": "1-domatio-3"
+        },
+        {
+          "yaw": -2.7198227548679466,
+          "pitch": 0.6549973447826893,
+          "rotation": 0,
+          "target": "3-hall-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-saloni-kouzina",
+      "name": "Saloni-Kouzina",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -1.5454968003696887,
+        "pitch": -0.02715454488322777,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8928432200529972,
+          "pitch": 0.3765024898388436,
+          "rotation": 0,
+          "target": "2-eisodos"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-domatio-1",
+      "name": "Domatio 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
