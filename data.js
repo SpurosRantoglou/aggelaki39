4ca596a0064cb5e164ef43587aa0774fe0fe8c5f
Spuros <spurakos98@gmail.com>
2020-10-26 15:14:33 +0200

    Second

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-domatio-2",
-      "name": "Domatio 2",
+      "id": "0-",
+      "name": "Είσοδος",
       "levels": [
         {
           "tileSize": 256,
@@ -24,237 +24,47 @@ var APP_DATA = {
       ],
       "faceSize": 1440,
       "initialViewParameters": {
-        "yaw": -2.7037762046266423,
-        "pitch": 0.883116485265818,
+        "yaw": -2.782784721724674,
+        "pitch": 0.12442963841171917,
         "fov": 1.3715802068843215
       },
       "linkHotspots": [
         {
-          "yaw": 1.7237225277436137,
-          "pitch": 0.5926592547699894,
+          "yaw": -2.3400820584023894,
+          "pitch": 0.6232402692629648,
           "rotation": 0,
-          "target": "4-hall-2"
+          "target": "4--3"
+        },
+        {
+          "yaw": -0.653245380278932,
+          "pitch": 0.3552546430783927,
+          "rotation": 0,
+          "target": "9-"
+        },
+        {
+          "yaw": 1.0711174995171096,
+          "pitch": 0.21856688251733658,
+          "rotation": 0,
+          "target": "1--1"
+        },
+        {
+          "yaw": 2.010710616173908,
+          "pitch": 0.5364390276340476,
+          "rotation": 0,
+          "target": "5--1"
+        },
+        {
+          "yaw": -1.985048334793989,
+          "pitch": 0.2511267940029924,
+          "rotation": 0,
+          "target": "7-"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "1-domatio-3",
-      "name": "Domatio 3",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 1440,
-      "initialViewParameters": {
-        "yaw": 0.91055350152239,
-        "pitch": 0.46513947057495564,
-        "fov": 1.3715802068843215
-      },
-      "linkHotspots": [
-        {
-          "yaw": 1.0046869356752257,
-          "pitch": 0.4633627444785908,
-          "rotation": 0,
-          "target": "4-hall-2"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "2-eisodos",
-      "name": "Eisodos",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 1440,
-      "initialViewParameters": {
-        "yaw": -1.245657052787342,
-        "pitch": 0.34369287213183597,
-        "fov": 1.3715802068843215
-      },
-      "linkHotspots": [
-        {
-          "yaw": -3.1059471749968672,
-          "pitch": 0.26634070329549075,
-          "rotation": 0,
-          "target": "5-saloni-kouzina"
-        },
-        {
-          "yaw": 0.34262274457766395,
-          "pitch": 0.270170887893153,
-          "rotation": 0,
-          "target": "3-hall-1"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "3-hall-1",
-      "name": "Hall 1",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 1440,
-      "initialViewParameters": {
-        "yaw": 0.4198253208910234,
-        "pitch": 0.501527493289263,
-        "fov": 1.3715802068843215
-      },
-      "linkHotspots": [
-        {
-          "yaw": -2.9596054341602027,
-          "pitch": 0.5185745132732578,
-          "rotation": 0,
-          "target": "2-eisodos"
-        },
-        {
-          "yaw": 0.6880058611257098,
-          "pitch": 0.4611098041216799,
-          "rotation": 0,
-          "target": "4-hall-2"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "4-hall-2",
-      "name": "Hall 2",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 1440,
-      "initialViewParameters": {
-        "yaw": -0.6850542102013186,
-        "pitch": 0.4741538895211761,
-        "fov": 1.3715802068843215
-      },
-      "linkHotspots": [
-        {
-          "yaw": -1.3074818283357175,
-          "pitch": 0.6099404718741823,
-          "rotation": 0,
-          "target": "0-domatio-2"
-        },
-        {
-          "yaw": 0.012815504858547655,
-          "pitch": 0.21945070367006458,
-          "rotation": 0,
-          "target": "1-domatio-3"
-        },
-        {
-          "yaw": -2.7198227548679466,
-          "pitch": 0.6549973447826893,
-          "rotation": 0,
-          "target": "3-hall-1"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "5-saloni-kouzina",
-      "name": "Saloni-Kouzina",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 1440,
-      "initialViewParameters": {
-        "yaw": -1.5454968003696887,
-        "pitch": -0.02715454488322777,
-        "fov": 1.3715802068843215
-      },
-      "linkHotspots": [
-        {
-          "yaw": 1.8928432200529972,
-          "pitch": 0.3765024898388436,
-          "rotation": 0,
-          "target": "2-eisodos"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "6-domatio-1",
-      "name": "Domatio 1",
+      "id": "1--1",
+      "name": "Υπνοδωμάτιο 1",
       "levels": [
         {
           "tileSize": 256,
@@ -280,14 +90,379 @@ var APP_DATA = {
         "yaw": 0,
         "fov": 1.5707963267948966
       },
-      "linkHotspots": [],
+      "linkHotspots": [
+        {
+          "yaw": 2.724854625227164,
+          "pitch": 0.5199582401496379,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2--2",
+      "name": "Υπνοδωμάτιο 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 2.4566042681385536,
+        "pitch": 0.4652116670894273,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7254479280477106,
+          "pitch": 0.5316868635506218,
+          "rotation": 0,
+          "target": "6--2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3--3",
+      "name": "Υπνοδωμάτιο 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 2.3435186544116355,
+        "pitch": 0.2754247245078716,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.015465633565821,
+          "pitch": 0.48435981921998206,
+          "rotation": 0,
+          "target": "6--2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4--3",
+      "name": "Διάδρομος 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -3.0445488414190685,
+        "pitch": 0.18729166687443666,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.983830008742965,
+          "pitch": 0.4004059452761002,
+          "rotation": 0,
+          "target": "7-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5--1",
+      "name": "Διάδρομος 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.7286268348761116,
+        "pitch": 0.581970884119464,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6178464595464455,
+          "pitch": 0.3817599816564936,
+          "rotation": 0,
+          "target": "6--2"
+        },
+        {
+          "yaw": -3.027512844160068,
+          "pitch": 0.7070005995094313,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": -3.0625525817365684,
+          "pitch": 0.3231342832599253,
+          "rotation": 0,
+          "target": "7-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6--2",
+      "name": "Διάδρομος 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -0.43633231299859077,
+        "pitch": 0.2940429751529585,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.267859814260147,
+          "pitch": 0.6242890595786932,
+          "rotation": 0,
+          "target": "2--2"
+        },
+        {
+          "yaw": -0.011346024883019723,
+          "pitch": 0.27531299583421465,
+          "rotation": 0,
+          "target": "3--3"
+        },
+        {
+          "yaw": -2.6208948146755695,
+          "pitch": 0.8085714778826194,
+          "rotation": 0,
+          "target": "5--1"
+        },
+        {
+          "yaw": 3.085905934810194,
+          "pitch": 0.3764060091726513,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-",
+      "name": "Σαλόνι",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -2.8379738976601594,
+        "pitch": 0.1671752624236298,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.18458733591493548,
+          "pitch": 0.15489455664587481,
+          "rotation": 0,
+          "target": "8-"
+        },
+        {
+          "yaw": 1.9951497609728746,
+          "pitch": 0.7509886276745945,
+          "rotation": 0,
+          "target": "4--3"
+        },
+        {
+          "yaw": 1.5786521246533436,
+          "pitch": 0.40152605471514136,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": 1.6976158401705987,
+          "pitch": 0.13125887965294325,
+          "rotation": 0,
+          "target": "5--1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-",
+      "name": "Κουζίνα",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.48990949599900624,
+        "pitch": 0.266536795396938,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.043528148953733,
+          "pitch": 0.50929302078654,
+          "rotation": 0,
+          "target": "7-"
+        },
+        {
+          "yaw": 1.968496852283276,
+          "pitch": 0.345526771723776,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-",
+      "name": "Μπάνιο",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 2.1391432584932843,
+        "pitch": 0.3616452506009331,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9283714023964933,
+          "pitch": 0.33291977202662437,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
       "infoHotspots": []
     }
   ],
-  "name": "Project Title",
+  "name": "MyNextHome - Αγγελάκη 39",
   "settings": {
     "mouseViewMode": "drag",
-    "autorotateEnabled": true,
+    "autorotateEnabled": false,
     "fullscreenButton": true,
     "viewControlButtons": true
   }
